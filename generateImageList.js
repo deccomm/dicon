fc58428a5ba6959f5b7934e5df96853b90c45fe9
designbasekorea <designbasekorea@gmail.com>
2024-05-16 16:34:21 +0900

    수정

--- generateImageList.js
+++ generateImageList.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+// 'assets' 폴더가 저장된 로컬 디렉토리 경로 설정
 const assetsDir = path.join(__dirname, 'assets');
-const outputFilePath = path.join(__dirname, 'assets', 'imageList.json');
+// 결과를 저장할 JSON 파일의 경로 설정
+const outputFilePath = path.join(__dirname, 'assets', 'logoList.json');
 
-let imageList = {};
+let logoList = {};
 
-// assets 폴더 내의 모든 디렉토리를 순회
+// 'assets' 폴더 내의 모든 디렉토리를 순회
 fs.readdirSync(assetsDir, { withFileTypes: true }).forEach(dir => {
     if (dir.isDirectory()) {
         const dirPath = path.join(assetsDir, dir.name);
-        imageList[dir.name] = fs.readdirSync(dirPath)
-            .filter(file => /\.(jpg|jpeg|png|svg)$/.test(file))
-            .map(file => `https://cdn.jsdelivr.net/gh/deccomm/fdp/assets/${dir.name}/${file}`);
+        logoList[dir.name] = fs.readdirSync(dirPath)
+            .filter(file => /\.svg$/.test(file))  // 오직 SVG 파일만 필터링
+            .map(file => `https://cdn.jsdelivr.net/gh/deccomm/dicon/assets/${dir.name}/${file}`);
     }
 });
 
-// JSON 파일로 결과 저장
-fs.writeFileSync(outputFilePath, JSON.stringify(imageList, null, 2));
-console.log('Image list JSON file generated.');
+// 결과를 JSON 파일로 저장
+fs.writeFileSync(outputFilePath, JSON.stringify(logoList, null, 2));
+console.log('Logo list JSON file generated.');
